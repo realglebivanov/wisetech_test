4ef4946ca8247b6d0082da3aca43c87b2ac47fb1
Gleb Ivanov <ivanov@panzerdog.com>
2024-01-31 10:35:12 +0200

    Finished working on a basic transform impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use anyhow::anyhow;
 
 struct State<'a> {
-    depth: usize,
     width: usize,
     line_width: usize,
     is_first_line: bool,
@@ -12,24 +11,42 @@ struct State<'a> {
 impl<'a> State<'a> {
     fn new(line_width: usize) -> Self {
         Self {
-            depth: 0,
             width: 0,
             line_width,
             is_first_line: true,
             lookbehind: Vec::with_capacity(line_width),
-            acc: String::with_capacity(line_width * 10),
+            acc: String::with_capacity(line_width * 4 * 10),
         }
     }
 
+    #[inline(always)]
+    fn add_word(mut self, word: &'a str) -> anyhow::Result<Self> {
+        let char_count = word.chars().count();
+        let width_delta = char_count + 1;
+
+        if char_count >= self.line_width {
+            return Err(anyhow!("Unexpected word size (longer than line_width)"));
+        }
+
+        if self.width + width_delta - 1 > self.line_width {
+            self.consume_line();
+        }
+
+        self.lookbehind.push(word);
+        self.width += width_delta;
+
+        Ok(self)
+    }
+
+    #[inline(always)]
     fn consume_line(&mut self) {
-        let spacing_left = self.line_width - self.width;
-        let (even_spacing, odd_spacer_count) = if self.depth == 1 {
-            (spacing_left, 0)
+        let spacing_left = self.line_width - (self.width - 1);
+        let init_length = self.lookbehind.len() - 1;
+
+        let (even_spacing, odd_spacer_count) = if self.lookbehind.len() == 1 {
+            (spacing_left - 1, 0)
         } else {
-            (
-                spacing_left / (self.depth - 1),
-                spacing_left % (self.depth - 1),
-            )
+            (spacing_left / init_length, spacing_left % init_length)
         };
 
         if !self.is_first_line {
@@ -37,22 +54,25 @@ impl<'a> State<'a> {
         }
 
         self.lookbehind
-            .drain(0..self.depth)
+            .drain(0..self.lookbehind.len())
             .enumerate()
-            .for_each(|(index, word)| {
-                self.acc.extend(word.chars());
+            .for_each(|(i, word)| {
+                self.acc.push_str(word);
 
-                for _ in 0..even_spacing {
+                if i == 0 || i < init_length {
                     self.acc.push(' ');
+
+                    for _ in 0..even_spacing {
+                        self.acc.push(' ');
+                    }
                 }
 
-                if index < odd_spacer_count {
+                if i < odd_spacer_count {
                     self.acc.push(' ');
                 }
             });
 
         self.is_first_line = false;
-        self.depth = 0;
         self.width = 0;
     }
 }
@@ -65,36 +85,13 @@ pub fn transform(input: &str, line_width: u32) -> anyhow::Result<String> {
     let mut state = input
         .split(" ")
         .filter(|w| !w.is_empty())
-        .try_fold(State::new(line_width), folder)?;
+        .try_fold(State::new(line_width), State::add_word)?;
 
-    if state.depth > 0 {
+    if state.lookbehind.len() > 0 {
         state.consume_line();
     }
 
     return Ok(state.acc);
-}
-
-fn folder<'a>(mut state: State<'a>, word: &'a str) -> anyhow::Result<State<'a>> {
-    let char_count = word.chars().count();
-    let width_delta = if state.lookbehind.is_empty() {
-        char_count
-    } else {
-        char_count + 1
-    };
-
-    if char_count >= state.line_width {
-        return Err(anyhow!("Unexpected word size (longer than line_width)"));
-    }
-
-    if state.width + width_delta > state.line_width {
-        state.consume_line();
-    }
-
-    state.lookbehind.push(word);
-    state.depth += 1;
-    state.width += width_delta;
-
-    Ok(state)
 }
 
 #[cfg(test)]
@@ -109,6 +106,7 @@ mod tests {
             ("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
              "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      "),
             ("Lorem     ipsum    dolor", 17, "Lorem ipsum dolor"),
+            ("象 形 字", 8, "象   形  字")
         ];
 
         for &(input, line_width, expected) in &test_cases {
